be04eb27e32e18564deb29bb7c682acda5276838
RISHABH <rboy29946@gmail.com>
2026-08-19 13:28:28 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,50 @@
 import { Container } from "@/components/ui/Container";
+import { CTASection } from "@/components/sections/CTASection";
+import { ProcessSection } from "@/components/sections/ProcessSection";
+import { ProjectsSection } from "@/components/sections/ProjectsSection";
+import { ServicesSection } from "@/components/sections/ServicesSection";
+import { TestimonialsSection } from "@/components/sections/TestimonialsSection";
+import { WhyChooseUsSection } from "@/components/sections/WhyChooseUsSection";
+import { Footer } from "@/components/layout/Footer";
+import { SiteNavigation } from "@/components/layout/SiteNavigation";
+import { Hero } from "@/components/sections/Hero";
+import { TrustBar } from "@/components/sections/TrustBar";
+
+export default function HomePage() {
+  return (
+    <>
+      <SiteNavigation />
+
+      <main>
+        {/* Hero */}
+        <Hero />
+
+        {/* Trust & credibility */}
+        <TrustBar />
+
+        {/* Core digital marketing services */}
+        <ServicesSection />
+
+        {/* How we work */}
+        <ProcessSection />
+
+        {/* Selected projects / case studies */}
+        <ProjectsSection />
+
+        {/* Why The Media Buddy */}
+        <WhyChooseUsSection />
+
+        {/* Client experiences */}
+        <TestimonialsSection />
+
+        {/* Final conversion section */}
+        <CTASection />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
 
 export default function Home() {
   return (
